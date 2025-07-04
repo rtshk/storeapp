e44df37f2ca868641f2fb1368d3a2b346e79289e
rtshk <riteshnzee@gmail.com>
2025-07-04 22:51:57 +0530

    enabled images access

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ifzagjrxuyxvjccbwsfu.supabase.co", // your Supabase project domain
+        pathname: "/storage/v1/object/public/**",     // allow all images in public storage
+      },
+    ],
+  },
 };
 
 export default nextConfig;
